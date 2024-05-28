7c1652b6e43441ad4a185e20707f5dc87347d52f
Jacob <jbale444@outlook.com>
2024-05-28 07:08:03 +0100

    Added print statement to test environment variable loading

--- server/app.js
+++ server/app.js
@@ -11,7 +11,6 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
-console.log('URI: ' + config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
--- server/utils/config.js
+++ server/utils/config.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+console.log('Env ' + process.env)
+
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
